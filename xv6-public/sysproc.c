357a33a9b0b1bbd5874448862af9fc1e74045aab
Adam Liu <adamliu@silber-courses.cis.udel.edu>
2022-10-26 13:13:09 +0000

    readded xv6 dir

---
+++ xv6-public/sysproc.c
@@ -0,0 +1,123 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+struct 
+{
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+sys_crsp(void)
+{
+  struct proc *p;
+  acquire(&ptable.lock);
+
+  cprintf("name\tpid\tstate\n\n");
+  cprintf("------------------------\n");
+
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      switch (p->state)
+    {
+    case RUNNING:
+      cprintf("%s\t%d\tRUNNING\n", p->name, p->pid);
+      break;
+    case SLEEPING:
+      cprintf("%s\t%d\tSLEEPING\n", p->name, p->pid);
+      break;
+    default:
+      break;
+    }
+    }
+  release(&ptable.lock);
+  return 0;
+}
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
